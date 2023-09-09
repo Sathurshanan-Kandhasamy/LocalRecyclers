bce7d3ef5e813a78db3bc8cc33c9fd792ba281e5
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-09-09 16:40:13 +1000

    JavaDoc files.

---
+++ JavaDoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FileManager"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"MainForm"},{"p":"<Unnamed>","l":"Recycler"},{"p":"<Unnamed>","l":"UIBuilderLibrary"}];updateSearchResults();
