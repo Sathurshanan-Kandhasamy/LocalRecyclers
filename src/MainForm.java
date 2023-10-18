7037b3b9acf76cc4835e125d295bfd04aab66516
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2023-10-18 23:04:41 +1000

    Update MainForm import statements.

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.util.Arrays;
 
 /**
